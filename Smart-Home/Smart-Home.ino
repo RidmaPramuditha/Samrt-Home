c4122117cbf824700f85a4cadc25b162ed0f592f
Pramuditha <49787217+RidmaPramuditha@users.noreply.github.com>
2026-01-11 14:33:48 +0530

    OLED Display

--- Smart-Home/Smart-Home.ino
+++ Smart-Home/Smart-Home.ino
@@ -1,3 +1,7 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 //pins
 int ldrPin = 2;
 int ledPin = 4;
@@ -13,6 +17,8 @@ int val = 0;
 int valTwo = 0;
 float sensorValueMQ2;
 
+Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 64, &Wire);
+
 void setup() {
   Serial.begin(9600);
   pinMode(ldrPin, INPUT);       // declare LDR Sensor as input
@@ -22,6 +28,7 @@ void setup() {
   pinMode(pirPinTwo, INPUT);       // declare PIR Sensor as input
   Serial.println("MQ2 warming up!");
   Serial.begin(9600);
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 }
 
 void  loop() {
@@ -40,6 +47,14 @@ void ldrSensor() {
 
   if (ldrValue == HIGH) {
     digitalWrite(ledPin, HIGH);
+    oled.clearDisplay();
+    oled.setTextColor(WHITE);
+    oled.setTextSize(1);
+    oled.setCursor(1, 0);
+    oled.print("Light Turn On");
+    oled.setTextSize(2);
+    oled.display();
+    oled.clearDisplay();
   }
   else {
     digitalWrite(ledPin, LOW);
@@ -62,6 +77,14 @@ void pirSensor() {
   {
     digitalWrite(buzzerPin, HIGH);
     delay(500);
+    oled.clearDisplay();
+    oled.setTextColor(WHITE);
+    oled.setTextSize(1);
+    oled.setCursor(1, 0);
+    oled.print("Motion Detected");
+    oled.setTextSize(2);
+    oled.display();
+    oled.clearDisplay();
   }
   else
   {
@@ -75,10 +98,18 @@ void gasdetector() {
   sensorValueMQ2 = analogRead(MQ2pin);  // read analog input pin 0
   Serial.print("Sensor Value: ");
   Serial.print(sensorValueMQ2);
-  
+
   if (sensorValueMQ2 > Threshold) {
     Serial.print(" | Gas detected!");
     digitalWrite(buzzerPin, HIGH);
+    oled.clearDisplay();
+    oled.setTextColor(WHITE);
+    oled.setTextSize(1);
+    oled.setCursor(1, 0);
+    oled.print("Gas Detected");
+    oled.setTextSize(2);
+    oled.display();
+    oled.clearDisplay();
   }
   Serial.println("");
   delay(2000);  // wait 2s for next reading
